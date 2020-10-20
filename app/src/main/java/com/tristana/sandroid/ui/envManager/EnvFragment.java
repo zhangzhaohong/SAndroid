60c6b0497c56b91e75ea1e9a3239ad4f28c6b7f8
zhangzhaohong <zhangzhaohong@kuaishou.com>
2020-10-20 22:38:32 +0800

    ft:环境管理

---
+++ app/src/main/java/com/tristana/sandroid/ui/envManager/EnvFragment.java
@@ -0,0 +1,34 @@
+package com.tristana.sandroid.ui.envManager;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.tristana.sandroid.R;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+public class EnvFragment extends Fragment {
+
+    private EnvViewModel envViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        envViewModel =
+                ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().getApplication()).create(EnvViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_env_manager, container, false);
+        envViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+
+            }
+        });
+        return root;
+    }
+}
