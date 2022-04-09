1d81e852c89f0f8782c79bac498dbe16e325f2ed
KoalaBear <owen000814@outlook.com>
2022-04-09 12:33:54 +0800

    feat: update

--- app/src/main/java/com/tristana/sandroid/model/illegalManager/PageType.java
+++
@@ -1,6 +0,0 @@
-package com.tristana.sandroid.model.illegalManager;
-
-public class PageType {
-    public static final String TYPE_PIC = "TYPE_PIC";
-    public static final String TYPE_VID = "TYPE_VID";
-}
