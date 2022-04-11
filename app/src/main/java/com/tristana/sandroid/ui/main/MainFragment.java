1c7af0842b4e3e14cc2c6aee4aa476f3646ceae2
KoalaBear <owen000814@outlook.com>
2022-04-11 13:58:14 +0800

    feat: update

--- app/src/main/java/com/tristana/sandroid/ui/main/MainFragment.java
+++ app/src/main/java/com/tristana/sandroid/ui/main/MainFragment.java
@@ -1,4 +1,4 @@
-package com.tristana.sandroid.ui.home;
+package com.tristana.sandroid.ui.main;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -6,26 +6,25 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.tristana.sandroid.R;
-
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-public class HomeFragment extends Fragment {
+import com.tristana.sandroid.R;
+import com.tristana.sandroid.ui.home.HomeViewModel;
 
-    private HomeViewModel homeViewModel;
+public class MainFragment extends Fragment {
+
+    private MainViewModel mainViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        if (homeViewModel == null)
-            homeViewModel =
-                    ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().getApplication()).create(HomeViewModel.class);
+        if (mainViewModel == null)
+            mainViewModel =
+                    ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().getApplication()).create(MainViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        mainViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 }
