ff343e60d9b8cbcc09024da9c87c4a2d387780b5
KoalaBear <owen000814@outlook.com>
2022-04-19 17:25:37 +0800

    feat: update

--- app/src/main/java/com/tristana/sandroid/ui/downloader/holder/DownloadTaskHolder.kt
+++ app/src/main/java/com/tristana/sandroid/ui/downloader/holder/DownloadTaskHolder.kt
@@ -5,8 +5,6 @@ import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
 import android.text.format.Formatter
 import android.view.View
-import android.view.ViewGroup
-import android.widget.LinearLayout
 import androidx.appcompat.widget.AppCompatTextView
 import com.airbnb.epoxy.EpoxyAttribute
 import com.airbnb.epoxy.EpoxyHolder
