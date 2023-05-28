ef926be33264dc49f34dfb87f0a807d7822fc7c1
KoalaBear <owen000814@outlook.com>
2023-05-28 14:10:28 +0800

    feat: update

--- tristanaLibrary/src/main/java/com/tristana/library/view/popupWindow/PopupWindowView.kt
+++ tristanaLibrary/src/main/java/com/tristana/library/view/popupWindow/PopupWindowView.kt
@@ -14,16 +14,8 @@ import razerdp.util.animation.TranslationConfig
 class PopupWindowView(context: Context?) : BasePopupWindow(context) {
     private lateinit var textView: AppCompatTextView
 
-    /**
-     *
-     *
-     * 返回一个contentView以作为PopupWindow的contentView
-     *
-     * <br></br>
-     * **强烈建议使用[BasePopupWindow.createPopupById]，该方法支持读取View的xml布局参数，否则可能会出现与布局不一样的展示从而必须手动传入宽高等参数**
-     */
-    override fun onCreateContentView(): View {
-        return createPopupById(R.layout.item_popup_layout)
+    init {
+        setContentView(R.layout.item_popup_layout)
     }
 
     override fun onViewCreated(contentView: View) {
