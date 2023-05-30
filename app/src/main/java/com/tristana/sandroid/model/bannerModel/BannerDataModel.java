e3b09a2bdb7f5a1024206957e096d490cbf04148
KoalaBear <owen000814@outlook.com>
2023-05-30 12:05:04 +0800

    feat: update

--- app/src/main/java/com/tristana/sandroid/model/bannerModel/BannerDataModel.java
+++ app/src/main/java/com/tristana/sandroid/model/bannerModel/BannerDataModel.java
@@ -1,11 +1,14 @@
 package com.tristana.sandroid.model.bannerModel;
 
+import lombok.Data;
+
 /**
  * @author koala
  * @version 1.0
  * @date 2022/4/12 18:17
  * @description
  */
+@Data
 public class BannerDataModel {
     private final String imagePath;
 
