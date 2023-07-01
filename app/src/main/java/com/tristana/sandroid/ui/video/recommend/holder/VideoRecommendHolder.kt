45c5e186cc428900a051da8e442c4ed4b2934bbe
KoalaBear <owen000814@outlook.com>
2023-07-01 14:43:54 +0800

    feat: update

--- app/src/main/java/com/tristana/sandroid/ui/video/recommend/holder/VideoRecommendHolder.kt
+++ app/src/main/java/com/tristana/sandroid/ui/video/recommend/holder/VideoRecommendHolder.kt
@@ -3,11 +3,15 @@ package com.tristana.sandroid.ui.video.recommend.holder
 import android.content.Context
 import android.view.View
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
 import butterknife.BindView
 import com.airbnb.epoxy.*
 import com.airbnb.epoxy.VisibilityState.FULL_IMPRESSION_VISIBLE
+import com.android.iplayer.base.AbstractMediaPlayer
 import com.android.iplayer.listener.OnPlayerEventListener
 import com.android.iplayer.media.IMediaPlayer
+import com.android.iplayer.media.core.ExoPlayerFactory
+import com.android.iplayer.media.core.IjkPlayerFactory
 import com.android.iplayer.model.PlayerState
 import com.android.iplayer.widget.VideoPlayer
 import com.android.iplayer.widget.WidgetFactory
@@ -15,7 +19,9 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.Priority
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
+import com.tristana.library.tools.sharedPreferences.SpUtils
 import com.tristana.sandroid.R
+import com.tristana.sandroid.dataModel.data.DataModel
 import com.tristana.sandroid.epoxy.holder.BaseEpoxyHolder
 import com.tristana.sandroid.epoxy.holder.CustomEpoxyModelWithHolder
 import com.tristana.sandroid.respModel.video.recommend.AwemeDataModel
@@ -36,6 +42,8 @@ abstract class VideoRecommendHolder : CustomEpoxyModelWithHolder<VideoRecommendH
 
     @EpoxyAttribute
     lateinit var item: AwemeDataModel
+
+    private var isFirstLoad = true
 
     override fun getViewType(): Int {
         return 0
@@ -67,8 +75,16 @@ abstract class VideoRecommendHolder : CustomEpoxyModelWithHolder<VideoRecommendH
 
     override fun bind(holder: Holder) {
         super.bind(holder)
-        holder.thumbView?.visibility = View.VISIBLE
-        holder.videoPlayer?.visibility = View.GONE
+        if (isFirstLoad) {
+            holder.thumbView?.visibility = View.VISIBLE
+            holder.videoPlayer?.visibility = View.GONE
+        }
+        item.author?.nickname?.let {
+            holder.authorView?.text = "@$it"
+        }
+        item.desc?.let {
+            holder.descView?.text = it
+        }
         if (item.video?.cover?.urlList?.isNotEmpty() == true) {
             holder.thumbView?.let {
                 val options = RequestOptions()
@@ -95,10 +111,23 @@ abstract class VideoRecommendHolder : CustomEpoxyModelWithHolder<VideoRecommendH
             item.author?.nickname?.let { it + "的作品" } ?: "无标题"
         }) //视频标题(仅横屏状态可见)
         holder.videoPlayer?.setLoop(true)
-        //设置播放源
-        val cachePath = PreloadManager.getInstance(context).getPlayUrl(item.videoPath)
-        holder.videoPlayer?.setDataSource(cachePath)
         holder.videoPlayer?.setOnPlayerActionListener(object : OnPlayerEventListener() {
+            override fun createMediaPlayer(): AbstractMediaPlayer {
+                return when (SpUtils.get(context, DataModel.VIDEO_TECH_SP, 0) as Int) {
+                    0, 1 -> {
+                        IjkPlayerFactory.create().createPlayer(context);
+                    }
+
+                    2 -> {
+                        ExoPlayerFactory.create().createPlayer(context);
+                    }
+
+                    else -> {
+                        super.createMediaPlayer()
+                    }
+                }
+            }
+
             override fun onPlayerState(state: PlayerState?, message: String?) {
                 super.onPlayerState(state, message)
                 when (state) {
@@ -106,10 +135,12 @@ abstract class VideoRecommendHolder : CustomEpoxyModelWithHolder<VideoRecommendH
                         holder.thumbView?.visibility = View.VISIBLE
                         holder.videoPlayer?.visibility = View.GONE
                     }
+
                     PlayerState.STATE_START, PlayerState.STATE_PLAY, PlayerState.STATE_ON_PLAY -> {
                         holder.thumbView?.visibility = View.GONE
                         holder.videoPlayer?.visibility = View.VISIBLE
                     }
+
                     PlayerState.STATE_MOBILE -> {}
                     PlayerState.STATE_ON_PAUSE, PlayerState.STATE_PAUSE -> {}
                     PlayerState.STATE_RESET, PlayerState.STATE_STOP, PlayerState.STATE_DESTROY -> {}
@@ -119,6 +150,10 @@ abstract class VideoRecommendHolder : CustomEpoxyModelWithHolder<VideoRecommendH
                 }
             }
         })
+        //设置播放源
+        val cachePath = PreloadManager.getInstance(context).getPlayUrl(item.videoPath)
+        holder.videoPlayer?.setDataSource(cachePath)
+        isFirstLoad = false
     }
 
     override fun unbind(holder: Holder) {
@@ -140,5 +175,13 @@ abstract class VideoRecommendHolder : CustomEpoxyModelWithHolder<VideoRecommendH
         @JvmField
         @BindView(R.id.video_recommend_cover_view)
         var thumbView: AppCompatImageView? = null
+
+        @JvmField
+        @BindView(R.id.video_recommend_author)
+        var authorView: AppCompatTextView?=null
+
+        @JvmField
+        @BindView(R.id.video_recommend_desc)
+        var descView: AppCompatTextView?=null
     }
 }
