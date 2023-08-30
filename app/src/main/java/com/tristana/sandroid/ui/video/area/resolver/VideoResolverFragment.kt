a465c95f2338e558294de80b4c7e37c09a6cb6e0
koala bear <owen000814@outlook.com>
2023-08-30 11:14:03 +0800

    feat: update

--- app/src/main/java/com/tristana/sandroid/ui/video/area/resolver/VideoResolverFragment.kt
+++ app/src/main/java/com/tristana/sandroid/ui/video/area/resolver/VideoResolverFragment.kt
@@ -1,17 +1,41 @@
 package com.tristana.sandroid.ui.video.area.resolver
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.Editable
+import android.text.SpannableStringBuilder
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import butterknife.BindView
 import butterknife.ButterKnife
+import com.blankj.utilcode.util.ClipboardUtils
+import com.blankj.utilcode.util.ToastUtils
 import com.tristana.sandroid.R
 
 class VideoResolverFragment : Fragment() {
 
     private var viewModel: VideoResolverViewModel? = null
+
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.video_resolver_link_input)
+    lateinit var inputLink: AppCompatEditText
+
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.video_resolver_clear)
+    lateinit var buttonClear: AppCompatButton
+
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.video_resolver_from_clipboard)
+    lateinit var buttonFromClipboard: AppCompatButton
+
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.video_resolver_operation)
+    lateinit var buttonResolverOperation: AppCompatButton
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -23,6 +47,28 @@ class VideoResolverFragment : Fragment() {
                 )
         val root = inflater.inflate(R.layout.fragment_video_resolver, container, false)
         ButterKnife.bind(this, root)
+        viewModel?.link?.observe(viewLifecycleOwner) { input ->
+            input?.let {
+                inputLink.text = SpannableStringBuilder(it)
+            }
+        }
+        buttonClear.setOnClickListener {
+            viewModel?.link?.value = ""
+        }
+        buttonFromClipboard.setOnClickListener { input ->
+            input?.let {
+                viewModel?.link?.value = ClipboardUtils.getText().toString()
+            }
+        }
+        buttonResolverOperation.setOnClickListener {
+            viewModel?.link?.value?.let { input ->
+                if (input.isEmpty()) {
+                    ToastUtils.showLong("分享链接不可为空")
+                } else {
+
+                }
+            }
+        }
         return root
     }
 
--- app/src/main/java/com/tristana/sandroid/ui/video/area/resolver/VideoResolverViewModel.kt
+++ app/src/main/java/com/tristana/sandroid/ui/video/area/resolver/VideoResolverViewModel.kt
@@ -1,6 +1,26 @@
 package com.tristana.sandroid.ui.video.area.resolver
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.tristana.library.tools.http.OkHttpRequestGenerator
+import com.tristana.sandroid.MyApplication
+import com.tristana.sandroid.http.PathCollection
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class VideoResolverViewModel : ViewModel() {
+
+    var link = MutableLiveData<String>(null)
+
+    var response = MutableLiveData<String>(null)
+
+    suspend fun doRequest() {
+        return withContext(Dispatchers.IO) {
+            OkHttpRequestGenerator.create(MyApplication.host, PathCollection.VIDEO_TIKTOK_API)
+                .addParam("link", link.value).get().sync()?.let { response ->
+
+                }
+        }
+    }
+
 }
